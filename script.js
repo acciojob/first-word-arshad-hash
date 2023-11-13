9068d463b75c4c3f6c1aa993d502fc77ca2a5c64
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-14 00:43:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
-  // your code here
-}
+    let r=s;
+    if(!r.includes(" ")){
+        return r;
+    }
+    // your code here
+    if(s.length==0){return "";}
+   
+     let red =s.trim().split(" ");
+     return (red[0]);
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+      
+  
+  }
+  
+  // Do not change the code below
+  
+   const s = prompt("Enter String:");
+   alert(firstWord(s));
